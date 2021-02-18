458266d20d6cdba5528cc3856fc41ca303baa409
Matt Bennett <matt.bennett91@gmail.com>
2021-02-18 18:08:53 +0100

    Add basic terraform

---
+++ infra/terraform.tf
@@ -0,0 +1,21 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }  
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "mrbtfstate"
+    container_name       = "tfstate"
+    key                  = "mhwrando.tfstate"
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
