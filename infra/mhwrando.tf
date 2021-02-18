a114dcf0fab4cfdc907860c8184b7ab13c45d806
Matt Bennett <matt.bennett91@gmail.com>
2021-02-18 18:51:07 +0100

    use correct name

--- infra/mhwrando.tf
+++ infra/mhwrando.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "mhwrando" {
 
 resource "azurerm_app_service_plan" "mhwrando" {
   name                = "mhwrando-appserviceplan"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.mhwrando.location
+  resource_group_name = azurerm_resource_group.mhwrando.name
 
   sku {
     tier = "Free"
@@ -16,8 +16,8 @@ resource "azurerm_app_service_plan" "mhwrando" {
 
 resource "azurerm_app_service" "mhwrando" {
   name                = "mhwrando-app-service"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.mhwrando.location
+  resource_group_name = azurerm_resource_group.mhwrando.name
   app_service_plan_id = azurerm_app_service_plan.mhwrando.id
 
   site_config {
